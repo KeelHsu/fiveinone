5cc3d23243b171cd55b0c43df7abb0d3f4630a29
Keel <keel.hsu@gmail.com>
2020-06-17 16:58:06 +0800

    棋盘重画，线粗细问题待解决

--- assets/components/board/index.tsx
+++ assets/components/board/index.tsx
@@ -7,13 +7,23 @@ import './index.less'
 const Board = () => {
   return <div className='square'>
     <div id='board' className='square-content'>
-      {boardDate.map((rows: Array<number>, row: number) => {
-        return rows.map((value: number, column: number) => {
-          return <div date-row={row} date-column={column} key={column}
-            className={`unit${row == 0 ? ' top' : ''}${row == boardDate.length - 1 ? ' bottom' : ''}${column == 0 ? ' left' : ''}${column == rows.length - 1 ? ' right' : ''}`}
-          />
-        })
-      })}
+      <div className='row-layer'>
+        {boardDate.map((rows: Array<number>, row: number) => {
+          return <div className='row-line' key={row} />
+        })}
+      </div>
+      <div className='col-layer'>
+        {boardDate[0].map((value: number, column: number) => {
+          return <div className='col-line' key={column} />
+        })}
+      </div>
+      <div className='date-layer'>
+        {boardDate.map((rows: Array<number>, row: number) => {
+          return rows.map((value: number, column: number) => {
+            return <div className='unit' date-row={row} date-column={column} key={column} />
+          })
+        })}
+      </div>
     </div>
   </div >
 }
