97d919f35e11896a77f875d8dabd5457023d4419
Keel <keel.hsu@gmail.com>
2020-06-18 15:39:57 +0800

    星位

--- assets/components/stage/index.tsx
+++ assets/components/stage/index.tsx
@@ -6,9 +6,26 @@ import './index.less'
 const controller = new Controller(15, 15)
 
 const Stage = () => {
+  controller.data[0][0] = 1;
+  controller.data[0][1] = 1;
+  controller.data[5][2] = 2;
+  controller.data[5][3] = 2;
+  controller.data[5][4] = 1;
+  controller.data[6][4] = 2;
+
   return <div className='square'>
     <div id='stage' className='square-content'>
-      <div className='grid-layer' />
+      <div className='grid-layer star-layer'>
+        <div className="corner"></div>
+        <div className="rest rest-mid"></div>
+        <div className="corner"></div>
+        <div className="rest rest-side"></div>
+        <div className="middle"></div>
+        <div className="rest rest-side"></div>
+        <div className="corner"></div>
+        <div className="rest rest-mid"></div>
+        <div className="corner"></div>
+      </div>
       <div className='date-layer'>
         {controller.data.map((rows: Array<number>, row: number) => {
           return rows.map((value: number, column: number) => {
